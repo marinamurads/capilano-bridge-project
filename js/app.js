7fb69f544449f2a6fc1cf0b40afa2ccff8de7ed0
Marina Murad <42791887+marinamurads@users.noreply.github.com>
2020-01-19 13:23:07 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelector(".nav-links li");
+const links = document.querySelector(".links");
 
-hamburger.addEventListener("click", () => {
+[hamburger, links].map(element => element.addEventListener("click", () => {
     navLinks.classList.toggle("open");
-});
+    links.classList.toggle("open");
+}));
